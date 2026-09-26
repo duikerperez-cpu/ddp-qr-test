94ff894a9876c8ab265418d3bdd4799df93392cd
Daniel Perez <duikerperez@gmail.com>
2026-09-26 15:17:34 -0300

    Update page.tsx

--- app/page.tsx
+++ app/page.tsx
@@ -1,7 +1,7 @@
 "use client"
 import { useState, useEffect } from "react"
 import { createClient } from "@supabase/supabase-js"
-
+1
 const supabase = createClient(
   process.env.NEXT_PUBLIC_SUPABASE_URL!,
   process.env.NEXT_PUBLIC_SUPABASE_ANON_KEY!
